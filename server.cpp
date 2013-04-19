d04fff8ea930a5574a8f0848a9da4f05bdd28989
Vic Hargrave <vichargrave@gmail.com>
2013-04-18 17:06:15 -0700

    Updated copyright info

--- server.cpp
+++ server.cpp
@@ -7,7 +7,7 @@
 
    ------------------------------------------
 
-   Copyright @ [Vic Hargrave - http://vichargrave.com]
+   Copyright © 2013 [Vic Hargrave - http://vichargrave.com]
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
