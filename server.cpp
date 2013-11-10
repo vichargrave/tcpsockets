e7d73a0ddaef0da01e1316fc2c69f5dba47907a6
vichargrave <vichargrave@gmail.com>
2013-11-10 09:09:49 -0800

    Fixed memory leak in server main program

--- server.cpp
+++ server.cpp
@@ -52,6 +52,7 @@ int main(int argc, char** argv)
                     printf("received - %s\n", line);
                     stream->send(line, len);
                 }
+                delete stream;
             }
         }
     }
