8df552cd7f290bf3801ffd0ca55f40112120ad90
Vic Hargrave <vichargrave@gmail.com>
2016-12-31 20:06:32 -0800

    Update header credits

--- tcpacceptor.cpp
+++ tcpacceptor.cpp
@@ -6,8 +6,8 @@
 
    ------------------------------------------
 
-   Copyright © 2013 [Vic Hargrave - http://vichargrave.com]
-
+   Copyright (c) 2013 Vic Hargrave
+   
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
