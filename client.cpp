80cfb38632bc48bee84b55aa971cb80a542e3948
Vic Hargrave <vichargrave@gmail.com>
2016-12-31 20:02:57 -0800

    Update header credits

--- client.cpp
+++ client.cpp
@@ -5,7 +5,7 @@
 
    ------------------------------------------
 
-   Copyright © 2013 [Vic Hargrave - http://vichargrave.com]
+   Copyright (c) 2013 Vic Hargrave
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
